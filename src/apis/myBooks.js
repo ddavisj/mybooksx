ba3baa71d97b0647c5d9f62ac3ab5c8ff82b293f
David Hack <davidhack11@gmail.com>
2022-04-08 12:29:23 +1000

    Added new API endpointto myBooks.js

--- src/apis/myBooks.js
+++ src/apis/myBooks.js
@@ -3,5 +3,6 @@
 import axios from 'axios';
 
 export default axios.create({
-   baseURL: 'http://localhost:3001',
+   // baseURL: 'http://localhost:3001',
+   baseURL: 'https://mybooks-api.herokuapp.com/',
 });
